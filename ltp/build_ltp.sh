1361684e8b70ad392a82b925bb2d1884abc24b58
vivi.li <vivi.li@analog.com>
2011-02-16 16:18:47 +0800

    LTP source can be built with beagle board.

--- ltp/build_ltp.sh
+++ ltp/build_ltp.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/sh
 
 if [ $# -ne 1 ]; then
        echo "Usage: $0 cvs_server_addr"
@@ -8,6 +8,11 @@ fi
 cvs_server_addr=$1
 
 LTP_SUB_DIR=ltp-full-20101031
+if [ -d $LTP_SUB_DIR ]
+then
+	rm -rf $LTP_SUB_DIR
+	echo "$0:	Clean directory"
+fi
 
 ## Ignore this step by svn ##
 # Checkout current file
@@ -35,13 +40,22 @@ CWD=`pwd`
 
 #echo "$0:	Get ltp working directory [$LTP_WORKING_DIR]"
 
+echo "$0:	Checking out from SVN, ltp/$LTP_SUB_DIR"
+#cvs -Q -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P $LTP_SUB_DIR
+svn -q co svn://$cvs_server_addr/ltp/trunk/$LTP_SUB_DIR
+if [ $? -ne 0 ]
+then
+	echo "$0:	Error, SVN checkout failed"
+	exit 1
+fi
+
 # Go to working directory
 echo "$0:	Go to working directory"
 cd $LTP_SUB_DIR
 
 # configure ltp
 echo "$0:	Configure ..."
-./configure --prefix=$CWD/$LTP_SUB_DIR --host=bfin-uclinux --build=i686-pc-linux-gnu
+./configure --prefix=$CWD/$LTP_SUB_DIR --host=arm-none-linux-gnueabi --build=i686-pc-linux-gnu > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
 	echo "$0:	Error, configure failed" 
@@ -60,12 +74,21 @@ fi
 
 # Build ltp testsuites
 echo "$0:	Make ..."
-make UCLINUX=1
+make UCLINUX=1 > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
 	echo "$0:	Error, make failed" 
 	exit 1
 fi
+
+echo "$0:	make install ..."
+make UCLINUX=1 install > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+	echo "$0:	Error, make install failed" 
+	exit 1
+fi
+echo "$0:	LTP build done"
 
 cd $CWD
 exit 0
