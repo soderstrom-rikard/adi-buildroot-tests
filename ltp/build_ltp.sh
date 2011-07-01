1dfa8b4bce4750948127c1c013a443eebec1217e
vivi.li <vivi.li@analog.com>
2011-07-01 10:17:19 +0800

    ltp: add blackfin support in ltp test; fix ltp test bug that uImage is not updated for beagle and panda boards.

--- ltp/build_ltp.sh
+++ ltp/build_ltp.sh
@@ -1,66 +1,53 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-       echo "Usage: $0 cvs_server_addr"
-       echo "Please append SVN repository ip address"
-       exit 1
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 cvs_server_addr arch_type"
+    echo "Please append SVN repository ip address and arch type"
+    exit 1
 fi       
-cvs_server_addr=$1
 
+cvs_server_addr=$1
+arch_type=$2
+CWD=`pwd`
 LTP_SUB_DIR=ltp-full-20101031
+
+
+# Remove old ltp source directory
 if [ -d $LTP_SUB_DIR ]
 then
-	rm -rf $LTP_SUB_DIR
-	echo "$0:	Clean directory"
+    rm -rf $LTP_SUB_DIR
+    echo "$0:	Clean directory"
 fi
 
-## Ignore this step by svn ##
-# Checkout current file
-#echo "$0:	Checking out from CVS, file [current]"
-#cvs -Q -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P ltp/current
-#if [ $? -ne 0 ]
-#then
-#	echo "$0:	Error, CVS checkout failed"
-#	exit 1
-#fi
-
-## We don't need patch for Makefile now ##
-# Checkout patch file for Makefiles 
-#echo "$0:	Checking out from CVS, file [Makefile.patch]"
-#cvs -Q -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P ltp/Makefile.patch
-#if [ $? -ne 0 ]
-#then
-#	echo "$0:	Error, CVS checkout failed"
-#	exit 1
-#fi
 
 # Checkout ltp source directory
-#LTP_WORKING_DIR=`cat ltp/current`
-CWD=`pwd`
-
-#echo "$0:	Get ltp working directory [$LTP_WORKING_DIR]"
-
 echo "$0:	Checking out from SVN, ltp/$LTP_SUB_DIR"
-#cvs -Q -d :pserver:anonymous@$cvs_server_addr:/cvsroot/uclinux533 co -A -P $LTP_SUB_DIR
 svn -q co svn://$cvs_server_addr/ltp/trunk/$LTP_SUB_DIR
 if [ $? -ne 0 ]
 then
-	echo "$0:	Error, SVN checkout failed"
-	exit 1
+    echo "$0:	Error, SVN checkout failed"
+    exit 1
 fi
+
 
 # Go to working directory
 echo "$0:	Go to working directory"
 cd $LTP_SUB_DIR
 
+
 # configure ltp
 echo "$0:	Configure ..."
-./configure --prefix=$CWD/$LTP_SUB_DIR/installation --host=arm-none-linux-gnueabi --build=i686-pc-linux-gnu > /dev/null 2>&1
+if [ $arch_type == "arm" ] ; then
+    ./configure --prefix=$CWD/$LTP_SUB_DIR/installation --host=arm-none-linux-gnueabi --build=i686-pc-linux-gnu > /dev/null 2>&1
+elif [ $arch_type == "blackfin" ] ; then
+    ./configure --prefix=$CWD/$LTP_SUB_DIR --host=bfin-uclinux --build=i686-pc-linux-gnu > /dev/null 2>&1
+fi
 if [ $? -ne 0 ]
 then
-	echo "$0:	Error, configure failed" 
-	exit 1
+    echo "$0:	Error, configure failed" 
+    exit 1
 fi
+
 
 ## We don't need patch for Makefile now ##
 # Patch for Makefiles
@@ -72,21 +59,34 @@ fi
 #	exit 1
 #fi
 
+
 # Build ltp testsuites
 echo "$0:	Make ..."
-make all > /dev/null 2>&1
-if [ $? -ne 0 ]
-then
-	echo "$0:	Error, make failed" 
-	exit 1
+if [ $arch_type == "arm" ] ; then
+    make all > /dev/null 2>&1
+elif [ $arch_type == "blackfin" ] ; then
+    make UCLINUX=1 > /dev/null 2>&1
 fi
 
-echo "$0:	make install ..."
-make install > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
-	echo "$0:	Error, make install failed" 
-	exit 1
+    echo "$0:	Error, make failed" 
+    exit 1
+fi
+
+
+# Install ltp testsuites
+echo "$0:	make install ..."
+if [ $arch_type == "arm" ] ; then
+    make install > /dev/null 2>&1
+elif [ $arch_type == "blackfin" ] ; then
+    make UCLINUX=1 install > /dev/null 2>&1
+fi
+
+if [ $? -ne 0 ]
+then
+    echo "$0:	Error, make install failed" 
+    exit 1
 fi
 echo "$0:	LTP build done"
 
