ef7a83cacebc3c8852fd36511ddf6de8e9cc9953
Grace Pan <grace.pan@analog.com>
2012-09-26 18:36:30 +0800

    [#6684] Update script: refine spimmc case.
    Signed-off-by: Grace Pan <grace.pan@analog.com>

--- common/runcp.sh
+++ common/runcp.sh
@@ -5,7 +5,7 @@ rm -rf /lib/libgfortran*
 rm -rf /lib/libthread*
 rm -rf /lib/libstdc*
 
-SUBDIR=`ls / | grep -v mnt | grep -v proc | grep -v sys | grep -v usr`
+SUBDIR=`ls / | grep -v mnt | grep -v proc | grep -v sys | grep -v usr | grep -v tmp`
 
 for i in $SUBDIR
 do
