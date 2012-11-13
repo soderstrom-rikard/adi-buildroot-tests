e628dd299573bf222f8b90fa2260e68ae40c43e5
Grace Pan <grace.pan@analog.com>
2012-11-13 17:25:35 +0800

    [#6684] Update script: refine board specific configure and update l2_app for bf561.
    Signed-off-by: Grace Pan <grace.pan@analog.com>

--- l2_app/helloworld.h
+++ l2_app/helloworld.h
@@ -1,5 +1,12 @@
+#ifdef BF561
+#define TEST_L2_LOW     0xfeb00000
+#define TEST_L2_HIGH    0xfeb20000
+#endif
+
+#ifdef BF609
 #define TEST_L2_LOW	0xc8080000
 #define TEST_L2_HIGH	0xc80c0000
+#endif
 
 int helloworld(char *str);
 
