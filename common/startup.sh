a1af5f1d078e41cc8df7f6f283b4e93a470373eb
Grace Pan <grace.pan@analog.com>
2013-02-19 17:14:44 +0800

    [#6684] Update script: update startup.sh.
    Signed-off-by: Grace Pan <grace.pan@analog.com>

--- common/startup.sh
+++ common/startup.sh
@@ -17,8 +17,7 @@ KERNEL_REPO_ADDR=git://$SERVER_ADDR/linux-kernel
 
 # Set branch info($GIT_BRANCH is got from MAIN_PROJ, TEST AND KERNEL INDEX MUST BE SAME WITH IT)
 
-echo $WORKSPACE $BUILD_ID $GIT_BRANCH > $WORKSPACE/../../../../logs/GIT_INFO
-
+echo $BUILD_ID $GIT_BRANCH > $WORKSPACE/../../../../logs/GIT_INFO
 MAIN_PROJ_INDEX=${GIT_BRANCH##*/}
 TEST_FRAME_INDEX=${GIT_BRANCH##*/}
 KERNEL_INDEX=${GIT_BRANCH##*/}
@@ -32,94 +31,94 @@ debug=0
 
 check_git_info ()
 {
-        echo "##########"
-            if [ -d $custom_workspace ] ; then
-                        cd $custom_workspace
-                                git branch
-                                        git remote -v
-                                            fi
+    echo "##########"
+    if [ -d $custom_workspace ] ; then
+        cd $custom_workspace
+        git branch
+        git remote -v
+    fi
 
 
-                                                if [ -d $custom_workspace/testsuites ] ; then
-                                                            cd $custom_workspace/testsuites
-                                                                    git branch
-                                                                            git remote -v
-                                                                                fi
+    if [ -d $custom_workspace/testsuites ] ; then
+        cd $custom_workspace/testsuites
+        git branch
+        git remote -v
+    fi
 
-                                                                                    if [ -d $custom_workspace/linux/linux-kernel ] ; then
-                                                                                                cd $custom_workspace/linux/linux-kernel
-                                                                                                        git branch
-                                                                                                                git remote -v
-                                                                                                                    fi
-                                                                                                                        echo "##########"
-                                                                                                                    }
+    if [ -d $custom_workspace/linux/linux-kernel ] ; then
+        cd $custom_workspace/linux/linux-kernel
+        git branch
+        git remote -v
+    fi
+    echo "##########"
+}
 
-                                                                                                                    if [ $debug -eq 1 ] ; then
-                                                                                                                            check_git_info
-                                                                                                                        fi
-
-
-                                                                                                                        # Checkout master project, switch to desired branch and get the latest source.
-                                                                                                                        cd $custom_workspace
-                                                                                                                        if [ "`git branch | grep -c \"$MAIN_PROJ_INDEX\"`" -eq 0 ] ; then
-                                                                                                                                git checkout -b $MAIN_PROJ_INDEX remotes/$MAIN_PROJ_REPO_NAME/$MAIN_PROJ_INDEX
-                                                                                                                            else
-                                                                                                                                    git checkout $MAIN_PROJ_INDEX
-                                                                                                                                fi
-
-                                                                                                                                git pull $MAIN_PROJ_REPO_NAME $MAIN_PROJ_INDEX
+if [ $debug -eq 1 ] ; then
+    check_git_info
+fi
 
 
-                                                                                                                                # Use local submodule git server
-                                                                                                                                cd $custom_workspace
-                                                                                                                                git submodule init
-                                                                                                                                if [ $use_local_src_server -eq 1 ] ; then
-                                                                                                                                        git config submodule.testsuites.url $TEST_FRAME_REPO_ADDR
-                                                                                                                                            git config submodule.linux/linux-kernel.url $KERNEL_REPO_ADDR
-                                                                                                                                        fi
-                                                                                                                                        git submodule update
+# Checkout master project, switch to desired branch and get the latest source.
+cd $custom_workspace
+if [ "`git branch | grep -c \"$MAIN_PROJ_INDEX\"`" -eq 0 ] ; then
+    git checkout -b $MAIN_PROJ_INDEX remotes/$MAIN_PROJ_REPO_NAME/$MAIN_PROJ_INDEX
+else
+    git checkout $MAIN_PROJ_INDEX
+fi
+
+git pull $MAIN_PROJ_REPO_NAME $MAIN_PROJ_INDEX
 
 
-                                                                                                                                        if [ $debug -eq 1 ] ; then
-                                                                                                                                                check_git_info
-                                                                                                                                            fi
+# Use local submodule git server
+cd $custom_workspace
+git submodule init
+if [ $use_local_src_server -eq 1 ] ; then
+    git config submodule.testsuites.url $TEST_FRAME_REPO_ADDR
+    git config submodule.linux/linux-kernel.url $KERNEL_REPO_ADDR
+fi
+git submodule update
 
 
-                                                                                                                                            # Checkout test frame and switch to desired branch.
-                                                                                                                                            cd $custom_workspace/testsuites
-                                                                                                                                            if [ "`git branch | grep -c \"$TEST_FRAME_INDEX\"`" -eq 0 ] ; then
-                                                                                                                                                    git checkout -b $TEST_FRAME_INDEX remotes/origin/$TEST_FRAME_INDEX
-                                                                                                                                                else
-                                                                                                                                                        git checkout $TEST_FRAME_INDEX
-                                                                                                                                                    fi
+if [ $debug -eq 1 ] ; then
+    check_git_info
+fi
 
 
-                                                                                                                                                    # Checkout kernel and switch to desired branch.
-                                                                                                                                                    cd $custom_workspace/linux/linux-kernel
-                                                                                                                                                    git checkout .
-                                                                                                                                                    if [ "`git branch | grep -c \"$KERNEL_INDEX\"`" -eq 0 ] ; then
-                                                                                                                                                            git checkout -b $KERNEL_INDEX remotes/origin/$KERNEL_INDEX
-                                                                                                                                                        else
-                                                                                                                                                                git checkout $KERNEL_INDEX
-                                                                                                                                                            fi
+# Checkout test frame and switch to desired branch.
+cd $custom_workspace/testsuites
+if [ "`git branch | grep -c \"$TEST_FRAME_INDEX\"`" -eq 0 ] ; then
+    git checkout -b $TEST_FRAME_INDEX remotes/origin/$TEST_FRAME_INDEX
+else
+    git checkout $TEST_FRAME_INDEX
+fi
 
 
-                                                                                                                                                            # Get the latest source for all submodules.
-                                                                                                                                                            cd $custom_workspace/testsuites
-                                                                                                                                                            git pull
-                                                                                                                                                            cd $custom_workspace/linux/linux-kernel
-                                                                                                                                                            git pull
-
-                                                                                                                                                            if [ $debug -eq 1 ] ; then
-                                                                                                                                                                    check_git_info
-                                                                                                                                                                fi
-
-
-                                                                                                                                                                # In WORKSPACE dir, make soft link to test log, so we can browse in hudson
-                                                                                                                                                                rm -fr $WORKSPACE/thisrun $WORKSPACE/logs
-                                                                                                                                                                ln -s /home/test/workspace/logs $WORKSPACE/logs
+# Checkout kernel and switch to desired branch.
+cd $custom_workspace/linux/linux-kernel
+git checkout .
+if [ "`git branch | grep -c \"$KERNEL_INDEX\"`" -eq 0 ] ; then
+    git checkout -b $KERNEL_INDEX remotes/origin/$KERNEL_INDEX
+else
+    git checkout $KERNEL_INDEX
+fi
 
 
-                                                                                                                                                                # Start regression test
-                                                                                                                                                                cd $custom_workspace/testsuites/common/
-                                                                                                                                                                ./run_kernel_test
+# Get the latest source for all submodules.
+cd $custom_workspace/testsuites
+git pull
+cd $custom_workspace/linux/linux-kernel
+git pull
+
+if [ $debug -eq 1 ] ; then
+    check_git_info
+fi
+
+
+# In WORKSPACE dir, make soft link to test log, so we can browse in hudson
+rm -fr $WORKSPACE/thisrun $WORKSPACE/logs
+ln -s /home/test/workspace/logs $WORKSPACE/logs
+
+
+# Start regression test
+cd $custom_workspace/testsuites/common/
+./run_kernel_test
